15bf85aa58976a3a59d144c64e78583cc45d46bc
luke-86 <luke-86@users.noreply.github.com>
2016-11-13 15:25:30 +0100

    refactor archiving functions

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 
 ### Settings ##
 BACKUPDIR="/mnt/backup"           ## Pfad zum Backupverzeichnis
-ROTATEDIR="/mnt/backup/rotate"    ## Pfad wo die Backups nach 30 Tagen konserviert werden
+ARCHIVEDIR="/mnt/backup/archive"    ## Pfad wo die Backups nach 30 Tagen konserviert werden
 TIMESTAMP="timestamp.dat"          ## Zeitstempel
 SOURCE="/share/CACHEDEV1_DATA/Public/ /share/snapshot"               ## Verzeichnis(se) welche(s) gesichert werden soll(en)
 DATUM="$(date +%d-%m-%Y)"          ## Datumsformat einstellen
@@ -48,53 +48,25 @@ fi
 
 
 ### Alle Variablen einlesen und letzte Backupdateinummer herausfinden ##
-set -- ${BACKUPDIR}/backup-???.tgz
-lastname=${!#}
-backupnr=${lastname##*backup-}
-backupnr=${backupnr%%.*}
-backupnr=${backupnr//\?/0}
-backupnr=$[10#${backupnr}]
+mkdir -p ${ARCHIVEDIR}
 
-### Backupdateinummer automatisch um +1 bis maximal 30 erhoehen ##
-if [ "$[backupnr++]" -ge 30 ]; then
-	mkdir -p ${ROTATEDIR}/${DATUM}-${ZEIT}
+### Test ob Backupverzeichnis existiert und Mail an Admin bei fehlschlagen ##
+if [ ! -d "${ARCHIVEDIR}" ]; then
 
-### Test ob Rotateverzeichnis existiert und Mail an Admin bei fehlschlagen ##
-	if [ ! -d "${ROTATEDIR}/${DATUM}-${ZEIT}" ]; then
-
-        	SUBJECT="Rotateverzeichnis nicht vorhanden!"
-        	TEXT="Die alten Backups konnten am ${DATUM} nicht verschoben werden. Das Verzeichnis ${ROTATEDIR} wurde nicht gefunden und konnte auch nicht angelegt werden."
-        	echo -e "To: $MAILTO \nFrom: $MAILFROM \nSubject: $SUBJECT \n\n $ANREDE\n\n $TEXT \n\n $SIGNATUR" | sendmail -t
-		exit 1
-	else
-		### alter Code: mv ${BACKUPDIR}/* ${ROTATEDIR}/${DATUM}-${ZEIT}  Damit verschiebt er die Dateien in sich selbst weil rotate ein Unterverzeichnis von backup ist. Es kommt zur Fehlermeldung ##
-		### /b* und /t* weil die Dateien nur mit b und t beginnen ##
-		mv ${BACKUPDIR}/b* ${ROTATEDIR}/${DATUM}-${ZEIT}
-		mv ${BACKUPDIR}/t* ${ROTATEDIR}/${DATUM}-${ZEIT}
-	fi
-
-	### Abfragen ob das Backupverschieben erfolgreich war ##
-	if [ $? -ne 0 ]; then
-        	SUBJECT="Backupverschieben fehlerhaft!"
-        	TEXT="Die alten Backups konnte am ${DATUM} nicht verschoben werden."
-        	echo -e "To: $MAILTO \nFrom: $MAILFROM \nSubject: $SUBJECT \n\n $ANREDE\n\n $TEXT \n\n $SIGNATUR" | sendmail -t
-
-		exit 1
-	else
-        	SUBJECT="Backupverschieben erfolgreich"
-	        TEXT="Die alten Backups wurde am ${DATUM} erfolgreich nach ${ROTATEDIR}/${DATUM}-${ZEIT} verschoben."
-        	echo -e "To: $MAILTO \nFrom: $MAILFROM \nSubject: $SUBJECT \n\n $ANREDE\n\n $TEXT \n\n $SIGNATUR" | sendmail -t
-
-		### die Backupnummer wieder auf 1 stellen ##
-		backupnr=1
-	fi
+        SUBJECT="Archivierungs-Verzeichnis nicht vorhanden!"
+        TEXT="Das Backup am ${DATUM} konnte nicht erstellt werden. Das Verzeichnis ${ARCHIVEDIR} wurde nicht gefunden und konnte auch nicht angelegt werden."
+        echo -e "To: $MAILTO \nFrom: $MAILFROM \nSubject: $SUBJECT \n\n $ANREDE\n\n $TEXT \n\n $SIGNATUR" | sendmail -t
+	exit 1
 fi
 
-backupnr=000${backupnr}
-backupnr=${backupnr: -3}
-filename=backup-${backupnr}.tgz
+### Backup-Archivierung für Datein die Aelter sind als 30 Tage ##
+find $BACKUPDIR -maxdepth 1 -mtime +30 -type f -exec mv "{}" $ARCHIVEDIR \;
 
-### Nun wird das eigentliche Backup ausgefuehrt ##
+### Archivierungs-Cleanup für Datein die Aelter sind als 60 Tage ##
+find $BACKUPDIR -maxdepth 1 -mtime +60 -type f -delete
+
+### Ausfuehren des eigentlichen Backups ##
+echo $DATUM >> $BACKUPLOG
 tar -cpzf ${BACKUPDIR}/${filename} -g ${BACKUPDIR}/${TIMESTAMP} ${SOURCE} ${EXCLUDE} >>$BACKUPLOG 2>&1
 
 ### Abfragen ob das Backup erfolgreich war ##
