3f6b24362fa469c377fe96319a037d89929a23f5
administrator <admin@QNAP-TS251.tbzhf.local>
2016-11-20 18:34:27 +0100

    prep for demo

--- testau002/testau002-db.sh
+++ testau002/testau002-db.sh
@@ -2,5 +2,6 @@
 
 ### Lukas' Script for Testing Testcase A/U-002 ###
 for i in {1..10}; do
-  /usr/local/mysql/bin/mysql -uroot -h127.0.0.1 -P200$i -proot_$i < testau002.sql | grep SELECT
+  echo -n "SELECT Query - Duration for Node $i in second in seconds: "
+  /usr/local/mysql/bin/mysql -uroot -h127.0.0.1 -P200$i -proot_$i < testau002.sql | grep SELECT | awk {'print $2'} 
 done
